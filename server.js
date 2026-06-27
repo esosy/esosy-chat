30397a021ebe27073082f6fcacd92ad2c9c4e068
esosy <yigithanindunyasi8@gmail.com>
2026-06-27 23:56:47 +0300

    Refactor server to use environment variable for PORT

--- server.js
+++ server.js
@@ -32,6 +32,8 @@ console.log("Kullanıcı çıktı:", socket.id);
 
 });
 
-server.listen(3000, () => {
-console.log("Esosy Chat çalışıyor: http://localhost:3000");
+const PORT = process.env.PORT || 3000;
+
+server.listen(PORT, () => {
+    console.log(`Esosy Chat çalışıyor: ${PORT}`);
 });
